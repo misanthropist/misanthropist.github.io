fbed07171fc7c4fcfd3c3beacb26eccb55c4adb0
doudou <2814143768@qq.com>
2021-01-30 22:10:03 +0800

    fixed bugs

--- cgi-bin/car.py
+++ cgi-bin/car.py
@@ -249,8 +249,8 @@ class Servo(object):
     def left_rotate(self):
         self.angles = self.get_angles()
         self.angles["arm2"] = self.angles["arm2"]+10
-        if self.angles["arm2"] >= 70:
-            self.angles["arm2"] = 40
+        if self.angles["arm2"] >= 110:
+            self.angles["arm2"] = 60
         rotate(self.arm2, self.angles["arm2"])
         self.set_angles()
         
